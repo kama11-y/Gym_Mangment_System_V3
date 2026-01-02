ae32bbc4e2c40a41d06c94acf0f7365b512fee15
Kamal <kamal@example.com>
2026-01-02 19:43:35 +0100

    Add Member Model

---
+++ Crud_GYM_System/Database/Models/Members.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f'Name : {self.name}\nAge : {self.age}'
--- Crud_GYM_System/Main.py
+++ Crud_GYM_System/Main.py
@@ -1,1 +1,3 @@
-print (11)
+from Database.Models.Members import Member
+x = Member(1,'kamal',23)
+print (x)
